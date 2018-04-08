b97620512e3ee80b41ba49b8418bb126c4c3469e
JeffreyRandom <35880312+JeffreyRandom@users.noreply.github.com>
2018-04-07 19:27:00 -0500

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,85 @@
+const Discord = require('discord.js');
+var bot = new Discord.Client();
+const Token = 'NDMxOTczMzQwMjE2MDMzMjkw.Darw9w.X_i1GMyKnhTvnYckrC7Pl5OY5I8';
+const Prefijo = 'j!'
+var cli = new Discord.Client({autoReconnect:true});
+
+var fortunas = [
+    'Sí',
+    'No',
+    'Tal vez',
+    'No c',
+    'Realmente no quiero responder eso :3'
+];
+
+bot.on('ready', function(listo) {
+    console.log("Listo!");
+});
+
+bot.on('ready', () => {
+    bot.user.setActivity('Di j!ayuda');
+  })
+
+
+
+bot.on('message', function(message) {
+    console.log(message.content);
+    if (message.content == "?prefijo"){
+        message.channel.send("El prefijo es " + Prefijo);
+    };
+
+    if (!message.content.startsWith(Prefijo)) return;
+    var args = message.content.substring(Prefijo.length).split(" ");
+
+    switch(args[0].toLowerCase()) {
+    //  C+V
+        case "ayuda":
+           var embed = new Discord.RichEmbed()
+           .addField("j!pregunta", "Pregúntame cualquier cosa y yo te responderé con sinceridad! Con respuestas de: Sí, No, Tal vez...")
+           .addField("j!info", "Te dice mi versión (Jeffrey Bot)")
+           .addField("j!llego", "Hago saber que tú estas conectado.")
+           .addField("j!links", "Envía un mensaje con los links de redes sociales y del server.")
+           .addField("?prefijo", "Te dice el prefijo para citar comandos.")
+           .addField("j!ayuda", "Te muestra este mensaje.")
+           .setColor(0x00ff15)
+           .setFooter("Versión 1.2 de ayuda.")
+           .setThumbnail(message.author.avatarURL)
+
+           message.author.send(embed);
+           message.channel.send("Mensaje enviado de forma privada!");
+           break;
+
+        case "info":
+            message.channel.send("*Versión de JeffreyBot: 1.3.0*")
+            break;
+
+        case "llego":
+            message.channel.send(message.author.toString() + " Ha llegado prros!")
+            break;
+
+        case "bola8":
+            message.channel.send("Algo ha ido mal, " + message.author.toString() + " " + "¡Ese comando ya no existe!" + " Ahora es j!pregunta");
+            break;
+        
+        case "pregunta":
+            if (args[1]) message.channel.send(message.author.toString() + " " + "La respuesta a tu pregunta es: " + fortunas[Math.floor(Math.random()* fortunas.length)]);
+            else message.channel.send("No pude leer eso ;-;");
+            break;
+
+        case "links":
+        var embed = new Discord.RichEmbed()
+        .addField("YouTube de Jeffrey", "https://www.youtube.com/channel/UCCYiF7GGja7iJgsc4LN0oHw", true)
+        .addField("Twitter de Jeffrey", "https://www.twitter.com/JeffreyGMD", true)
+        .addField("Página del server", "https://gmdsjeff.000webhostapp.com/")
+        .setColor(0x00ff15)
+        .setFooter("Versión 1.0 de links")
+        .setThumbnail(message.author.avatarURL)
+            message.channel.send(embed);
+            break;
+
+        default:
+            message.channel.send("Comando no existente!");
+            break;
+    }
+});
+bot.login(process.env.Token);
